9d72a61376d9a63b965ecf7bb62d94f792e7ea32
洪凯 <buqinuo1993@gmail.com>
2018-11-25 19:17:32 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    baseUrl: process.env.NODE_ENV === 'production' ? '/online/' : '/',
+    // outputDir: 在npm run build时 生成文件的目录 type:string, default:'dist'
+    // outputDir: 'dist',
+    // pages:{ type:Object,Default:undfind } 
+    configureWebpack:function(config){
+        return {
+          resolve: {
+            // extensions: ['.js', '.json'],
+            alias: {
+              '@': path.resolve(__dirname, './src')
+            }
+          }
+        }
+      },
+    devServer: {
+        port: 8888, // 端口号
+        host: 'localhost',
+        https: false, // https:{type:Boolean}
+        open: true, //配置自动启动浏览器
+        // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
+        proxy: {
+            '/api': {
+                target: '<url>',
+                ws: true,
+                changeOrigin: true
+            },
+            '/foo': {
+                target: '<other_url>'
+            }
+        },  // 配置多个代理
+    }
+}
